33c3ca8e734e0b59715b40e59676067bd74700f7
YagoTb <yagobecker13@gmail.com>
2025-06-13 20:18:45 -0300

    Logincrr

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const loginForm = document.getElementById('loginForm');
-    if (loginForm) {
-      loginForm.addEventListener('submit', function(e) {
-        e.preventDefault(); // Impede o envio real do formulário
-          if (document.getElementsByName('admin')) {
-            window.location.href = './gerenciador/pgdasboard/index.html'; // Redireciona para o dashboard
-          } else if (document.getElementsByName('user')) {
-            window.location.href = './usuario/index.html'; // Redireciona para o dashboard
-          }
-      });
-    }
-  });
+  const loginForm = document.getElementById('loginForm');
+  
+  if (loginForm) {
+    loginForm.addEventListener('submit', function(e) {
+      e.preventDefault();
+
+      // Pegue o valor do select
+      const select = document.getElementById('tipo_usuario');
+      const opcao = select.value;
+
+      if (opcao === 'admin') {
+        window.location.href = './gerenciador/pgdasboard/index.html';
+      } else if (opcao === 'user') {
+        window.location.href = './usuario/index.html';
+      }
+    });
+  }
+});
