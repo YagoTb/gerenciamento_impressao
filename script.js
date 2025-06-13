5e4d4c355910807954cd75f0a1adc939f8438f9f
YagoTb <yagobecker13@gmail.com>
2025-06-13 20:07:22 -0300

    Login

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
         e.preventDefault(); // Impede o envio real do formulário
           if (document.getElementsByName('admin')) {
             window.location.href = './gerenciador/pgdasboard/index.html'; // Redireciona para o dashboard
-          } else if (document.getElementsByName('usuario')) {
+          } else if (document.getElementsByName('user')) {
             window.location.href = './usuario/index.html'; // Redireciona para o dashboard
           }
       });
